89b3d2cb8c1962314a40b0927d2781b37d673402
Wasiullahaman <129711835+Wasiullahaman@users.noreply.github.com>
2025-07-13 16:01:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ uploaded_file = st.file_uploader("Upload CSV File", type=["csv"])
 if uploaded_file:
     data = pd.read_csv(uploaded_file)
 
+    # Drop 'Class' column if it exists
+    if 'Class' in data.columns:
+        data = data.drop(columns=['Class'])
+
     if 'Amount' in data.columns:
         data['Amount'] = scaler.transform(data[['Amount']])
 
